d604df68f4be87193c347370ac6c6f21bd418f52
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-10-04 11:55:43 -0300

    Projeto Fatorial

---
+++ fatorial/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fatorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFatorial_Click(object sender, RoutedEventArgs e)
+        {
+            double num, fat=1;
+            int cont = 1;
+
+            num = Convert.ToDouble(txtNumero.Text);
+
+            while (cont <= num)
+            {
+                fat = fat * cont;
+                cont++;
+            }
+            lblResultado.Content = fat.ToString();
+        }
+    }
+}
